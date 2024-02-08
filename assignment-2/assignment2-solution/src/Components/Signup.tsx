15402debd6e33aa67a6af59afb2127a203f283fb
shivkumarojha <ojhashivkumar88@gmail.com>
2024-02-08 19:24:48 +0530

    added some types for todo react app

--- assignment-2/assignment2-solution/src/Components/Signup.tsx
+++ assignment-2/assignment2-solution/src/Components/Signup.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import { useState } from 'react';
+import {Link} from 'react-router-dom';
 import {useSetRecoilState} from "recoil";
 import {authState} from "../store/authState.js";
 
+interface SingupData {
+    username: string;
+    password: string;
+}
 const Signup = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
